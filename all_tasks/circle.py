074b0fca666ac894cfc6684ec107558f43c05f28
MrFedko <miklerst@mail.ru>
2023-01-31 22:37:38 +0300

    circle area and long of circle

---
+++ all_tasks/circle.py
@@ -0,0 +1,28 @@
+from math import pi
+import decimal
+
+
+decimal.getcontext().prec = 42
+
+
+class Circle:
+    def __init__(self, diameter: float):
+        self.diameter: float = diameter
+        self.__circle_long: (decimal, None) = None
+        self.__area: (decimal, None) = None
+
+    @property
+    def diameter(self):
+        return self._diameter
+
+    @diameter.setter
+    def diameter(self, diameter: float):
+        self._diameter = diameter
+
+    @property
+    def circle_long(self) -> decimal:
+        return decimal.Decimal(pi * self.diameter if self.__circle_long is None else self.__circle_long)
+
+    @property
+    def area(self) -> decimal:
+        return decimal.Decimal(pi * pow(self.diameter / 2, 2) if self.__area is None else self.__area)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from all_tasks.triangle import Triangle
 from all_tasks.prime_or_composite import is_prime
 from all_tasks.binary_search import search
+from all_tasks.circle import Circle
 
 if __name__ == "__main__":
+    # Triangle
     a, b, c = (float(input(f"Введите {i + 1} сторону треугольника ")) for i in range(3))
     any_triangle = Triangle(a, b, c)
 
@@ -11,11 +13,16 @@ if __name__ == "__main__":
     except AttributeError:
         print("Попробуйте ещё раз")
 
-
+    #is_prime
     number: int = -1
     while number < 0 or number > 100_000:
         number = int(input("Введите число от 0 до 100_000 "))
     print(["Это составное число", "Это простое число"][is_prime(number)])
 
-
+    #search
     search()
+
+    #Circle
+    number: (float) = float(input("Введите диаметр круга: "))
+    krug = Circle(number)
+    print(f"Длинна окружности = {krug.circle_long:.42f} \nПлощадь круга = {krug.area:.42f}")
