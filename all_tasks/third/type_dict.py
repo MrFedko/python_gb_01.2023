23db2c155895d6f260989d0af1213de18c4505e4
MrFedko <miklerst@mail.ru>
2023-02-04 15:10:17 +0300

    get dict with keys - types

--- all_tasks/third/type_dict.py
+++ all_tasks/third/type_dict.py
@@ -1,6 +1,5 @@
 def get_type_dict(corteg: tuple) -> dict:
     result = {}
     for i in corteg:
-        result.setdefault(type(i), [])
-        result[type(i)].append(i)
+        result.setdefault(type(i), []).append(i)
     return result
