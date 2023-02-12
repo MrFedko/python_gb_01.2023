fc4d2606c68f2d372cb9ee0ff5f6a64d97915c4c
MrFedko <miklerst@mail.ru>
2023-02-12 16:46:30 +0300

    return dict where keys - chars, values - ord(char)

---
+++ all_tasks/fifth/dict_chr_ord.py
@@ -0,0 +1,2 @@
+def dict_chr_ord(text: str) -> dict[str:int]:
+    return {i: ord(i) for i in text}
--- all_tasks/fifth/main.py
+++ all_tasks/fifth/main.py
@@ -1,6 +1,11 @@
 from dict_with_int import dict_with_int
-
+from dict_chr_ord import dict_chr_ord
 
 if __name__ == "__main__":
     # dict with int
     print(dict_with_int(input("Введите от 4 чисел разделенных символом / ")))
+
+    print("*" * 50)
+
+    text = "Это текст для тестирования функции, где ключ - символ, а значение - его код."
+    print(dict_chr_ord(text))
