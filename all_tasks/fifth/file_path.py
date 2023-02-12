0bdb38f3e4210a54f36811b7ee24dea25bd11852
MrFedko <miklerst@mail.ru>
2023-02-12 18:19:57 +0300

    get absolut file path. return tuple(file path, name, extension)

---
+++ all_tasks/fifth/file_path.py
@@ -0,0 +1,7 @@
+import os
+
+
+def file_info(file_path):
+    path, filename = os.path.split(file_path)
+    name, extension = os.path.splitext(filename)
+    return path, name, extension
--- all_tasks/fifth/main.py
+++ all_tasks/fifth/main.py
@@ -3,33 +3,39 @@ from dict_chr_ord import dict_chr_ord
 from fizz_buzz import fizz_buzz
 from product_table import product_table
 from prime_gen import prime_gen
+from file_path import file_info
 
 if __name__ == "__main__":
-    # dict with int
-    print(dict_with_int(input("Введите от 4 чисел разделенных символом / ")))
-
+    # # dict with int
+    # print(dict_with_int(input("Введите от 4 чисел разделенных символом / ")))
+    #
+    # print("*" * 50)
+    #
+    # text = "Это текст для тестирования функции, где ключ - символ, а значение - его код."
+    # print(dict_chr_ord(text))
+    #
+    # print("*" * 50)
+    #
+    # my_dict = iter(dict_chr_ord(text).items())
+    # [print(next(my_dict)) for _ in range(5)]
+    #
+    # print("*" * 50)
+    #
+    # print([i for i in range(0, 101, 2) if i // 10 + i % 10 != 8])
+    #
+    # print("*" * 50)
+    #
+    # print(*fizz_buzz())
+    #
+    # print("*" * 50)
+    #
+    # product_table()
+    #
+    # print("*" * 50)
+    #
+    # [print(i) for i in prime_gen(int(input("Сколько простых чисел вывести? ")))]
+    #
     print("*" * 50)
 
-    text = "Это текст для тестирования функции, где ключ - символ, а значение - его код."
-    print(dict_chr_ord(text))
-
-    print("*" * 50)
-
-    my_dict = iter(dict_chr_ord(text).items())
-    [print(next(my_dict)) for _ in range(5)]
-
-    print("*" * 50)
-
-    print([i for i in range(0, 101, 2) if i // 10 + i % 10 != 8])
-
-    print("*" * 50)
-
-    print(*fizz_buzz())
-
-    print("*" * 50)
-
-    product_table()
-
-    print("*" * 50)
-
-    [print(i) for i in prime_gen(int(input("Сколько простых чисел вывести? ")))]
+    file_p = "/Users/mac/Desktop/GeekBrain/second_python/README.md"
+    print(file_info(file_p))
