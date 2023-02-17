0919fa1fe06370e8abc5cda557f527111bf7a5e0
MrFedko <miklerst@mail.ru>
2023-02-17 12:29:07 +0300

    date validate

---
+++ all_tasks/sixth/date_validate.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+def date_validate(date_text: str) -> bool:
+    try:
+        value = datetime.strptime(date_text, "%d.%m.%Y").date()
+        return True
+    except:
+        return False
+
+
+def _leap_info(date_text: str) -> bool:
+    year = int(date_text.split(".")[-1])
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return True
+    else:
+        return False
--- all_tasks/sixth/main.py
+++ all_tasks/sixth/main.py
@@ -1,5 +1,6 @@
-
+from date_validate import date_validate
 
 
 if __name__ == "__main__":
-    pass
+    print(date_validate("32.01.1989"))
+    print(date_validate("23.01.1989"))
